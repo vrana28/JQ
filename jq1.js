af729dbf7a7fdab506667e092c4f7c0b3075d862
vrana28 <45140739+vrana28@users.noreply.github.com>
2020-01-08 01:00:03 +0100

    Add files via upload

---
+++ jq1.js
@@ -0,0 +1,20 @@
+$("#prva").on("click",function(){
+
+    $("div").css("background-color","red");
+    $("#tekst").text("navigacija");
+
+});
+
+$("#druga").on("click",function(){
+
+    $("div").css("background-color","blue");
+    $("#tekst").text("preko");
+
+});
+
+$("#treca").on("click",function(){
+
+    $("div").css("background-color","orange");
+    $("#tekst").text("tabova");
+
+});
